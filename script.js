be03aa3cf69768cccaf45bfa888d1a9c453196ac
enoripl <tomek00@gmail.com>
2022-05-30 23:45:18 +0200

    displaying board

--- script.js
+++ script.js
@@ -18,16 +18,40 @@ const createPlayer = (playerName, playerMark) => {
 const gameFlow = (()=> {
     const addMark = (player, position) => {
         if (gameBoard.marks[position] == 'empty') {
-        gameBoard.marks.splice(position, 0, `${player.mark}`)
+        gameBoard.marks.splice(position, 1, `${player.mark}`)
     } else {console.log('Place is taken')};
+    displayController.updateBoard();
 }
     return {
         addMark
     }
 })();
 
+const displayController = (() => {
+    const newBoard = () => {
+        for (let i = 0; i < 9; i++) {
+            const boardContainer = document.querySelector('#boardContainer')
+            const divs = document.createElement('div');
+            divs.classList.add('box')
+            divs.setAttribute('id', `box${[i+1]}`)
+            boardContainer.appendChild(divs)
+        }
+    }; 
+    const updateBoard = () => {
+        const boxes = document.querySelectorAll(".box")
+        for (let i = 0; i < 9; i++) {
+            boxes[i].innerText = `${gameBoard.marks[i]}`
+        }
+    }
+    return {
+        newBoard,
+        updateBoard
+    };
+})();
+
 const jerzy = createPlayer('Jerzy', 'x')
-gameFlow.addMark(jerzy, 4)
+displayController.newBoard();
+gameFlow.addMark(jerzy, 1)
 const tomek = createPlayer('Tomek', 'o')
-gameFlow.addMark(tomek, 1)
+gameFlow.addMark(tomek, 8)
 console.log(gameBoard.marks) 
