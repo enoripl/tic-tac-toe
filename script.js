2d21b0c799fa87bac630bf27a3a06edefabc14ef
enoripl <tomek00@gmail.com>
2022-07-18 22:44:30 +0200

    implemented player switching

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+// gameboard object
 const gameBoard = (() => {
     const marks = ['','','','','','','','',''];
+    const winningMarks = [
+        [0,1,2]
+    ]
     const newBoard = () => {
         for (let i = 0; i < 9; i++) {
             const boardContainer = document.querySelector('#boardContainer')
@@ -18,47 +22,70 @@ const gameBoard = (() => {
     return {
         marks: marks,
         newBoard,
-        updateBoard
+        updateBoard,
+        winningMarks
     };
     
 })();
 
-const createPlayer = (name, marker) => {
-    return { 
-        name,
-        marker,
+// player object
+const player = (() => {
+    const createPlayer = (name, marker) => {
+        return { 
+            name,
+            marker,
+        }
     }
-};
+    return {
+        createPlayer,
+    }
+})();
 
-const gameFlow = (()=> {
+// game object
+const game = (()=> {
+    const tomek = player.createPlayer('Tomek', 'o');
+    const jerzy = player.createPlayer('Jerzy', 'x');
+    let currentPlayer = tomek;
+    function checkWinner() {
+        gameBoard.winningMarks.forEach((item, index) => {
+            if (gameBoard.marks[item[0]] === currentPlayer.marker && gameBoard.marks[item[1]] === currentPlayer.marker && gameBoard.marks[item[2]] === currentPlayer.marker) {
+                console.log('winner!')
+            }
+        })
+    }
+    // function nextPlayer() {
+    //     currentPlayer === tomek ? currentPlayer = jerzy : currentPlayer = tomek;
+    //     console.log('nextPlayer() function ran')
+    //     console.log('active player: ' + currentPlayer.name);
+    // }
     const addMark = (currentPlayer, position) => {
         const boxes = document.querySelectorAll('.box');
         boxes.forEach((box) => {
             box.addEventListener('click', () => {
                 const position = box.id;
                 if (gameBoard.marks[position] == '') {
-                gameBoard.marks.splice(position, 1, `${currentPlayer.marker}`);
-                gameBoard.updateBoard();
-            } else {console.log('Place is taken')};
+                    gameBoard.marks.splice(position, 1, `${currentPlayer.marker}`);
+                    gameBoard.updateBoard();
+                    console.log(currentPlayer.marker)
+                    console.log(gameBoard.marks) 
+                    checkWinner();
+                    if (currentPlayer === tomek) {
+                        currentPlayer = jerzy
+                    } else {
+                        currentPlayer = tomek
+                    }
+                } else {console.log('Place is taken')};
             })
         })
     }
-    const startGame = () => {
-        const startButton = document.querySelector('#startButton');
-        startButton.forEach((button) => {
-            button.addEventListener('click', () => {
-
-            })
-        })
-    }
+  
     return {
-        addMark
+        addMark,
+        currentPlayer,
+        checkWinner,
+        // nextPlayer
     }
 })();
 
-const jerzy = createPlayer('Jerzy', 'x')
 gameBoard.newBoard();
-const tomek = createPlayer('Tomek', 'o')
-gameBoard.updateBoard();
-console.log(gameBoard.marks) 
-gameFlow.addMark(tomek);
+game.addMark(game.currentPlayer);
